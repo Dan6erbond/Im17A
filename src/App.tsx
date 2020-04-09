f88bb7324be47de9d07c10eb1b0a5982a83b4022
Dan6erbond <moravrav@gmail.com>
2020-04-09 19:52:48 +0200

    Implement basic material ui from samples

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,228 @@
 import React from 'react';
 import './App.css';
-import {AppBar, Button, Container, createStyles, fade, IconButton, Theme, Toolbar, Typography} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
+import {
+    AppBar, Box,
+    Button, Container, createStyles,
+    CssBaseline, Divider, Fab,
+    fade,
+    IconButton, List, ListItem, ListItemIcon, ListItemText, SwipeableDrawer,
+    Theme,
+    Toolbar,
+    Typography, useScrollTrigger, WithStyles,
+    Zoom
+} from "@material-ui/core";
+import {withStyles} from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        grow: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            display: 'none',
-            [theme.breakpoints.up('sm')]: {
-                display: 'block',
-            },
-        },
-        search: {
-            position: 'relative',
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            '&:hover': {
-                backgroundColor: fade(theme.palette.common.white, 0.25),
-            },
-            marginRight: theme.spacing(2),
-            marginLeft: 0,
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(3),
-                width: 'auto',
-            },
-        },
-        searchIcon: {
-            padding: theme.spacing(0, 2),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        inputRoot: {
-            color: 'inherit',
-        },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('md')]: {
-                width: '20ch',
-            },
-        },
-        sectionDesktop: {
-            display: 'none',
-            [theme.breakpoints.up('md')]: {
-                display: 'flex',
-            },
-        },
-        sectionMobile: {
-            display: 'flex',
-            [theme.breakpoints.up('md')]: {
-                display: 'none',
-            },
-        },
-    }),
-);
+const drawerWidth = 240;
 
-function App() {
-    const classes = useStyles();
+const styles = (theme: Theme) => createStyles({
+    root: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+});
+
+interface ScrollTopProps {
+    children: React.ReactElement;
+}
+
+function ScrollTop(props: ScrollTopProps & WithStyles<typeof styles>) {
+    const {children} = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = (event: React.MouseEvent) => {
+        const anchor = ((event.target as HTMLDivElement).ownerDocument || document).querySelector(
+            '#back-to-top-anchor',
+        );
+
+        if (anchor) {
+            anchor.scrollIntoView({behavior: 'smooth', block: 'center'});
+        }
+    };
 
     return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-                    </Typography>
-                    <div className={classes.grow} />
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-
-            <Container maxWidth="md">
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button>
-            </Container>
-        </div>
+        <Zoom in={trigger}>
+            <div onClick={handleClick} role="presentation" className={props.classes.root}>
+                {children}
+            </div>
+        </Zoom>
     );
 }
 
-export default App;
+interface AppState {
+    open: boolean;
+}
+
+interface AppProps {
+}
+
+class App extends React.Component<AppProps & WithStyles<typeof styles>, AppState> {
+    constructor(props: AppProps & WithStyles<typeof styles>) {
+        super(props);
+
+        this.state = {open: false};
+    }
+
+    render() {
+        const toggleDrawer = (open: boolean) => {
+            this.setState({open: open});
+        };
+
+        const {classes} = this.props;
+
+        return (
+            <React.Fragment>
+                <CssBaseline/>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton onClick={() => toggleDrawer(true)} edge="start" className={classes.menuButton}
+                                    color="inherit" aria-label="menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            Im17A
+                        </Typography>
+                        <div className={classes.grow}/>
+                        <Button color="inherit">Home</Button>
+                    </Toolbar>
+                </AppBar>
+
+                <Toolbar id="back-to-top-anchor"/>
+
+                <Container maxWidth="md">
+                    <Box my={2}>
+                        {[...new Array(12)]
+                            .map(
+                                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                            )
+                            .join('\n')}
+                    </Box>
+
+                    <SwipeableDrawer
+                        anchor="left"
+                        open={this.state.open}
+                        onClose={() => toggleDrawer(false)}
+                        onOpen={() => toggleDrawer(true)}
+                        classes={{
+                            paper: classes.drawerPaper,
+                        }}
+                    >
+                        <div className={classes.toolbar} />
+                        <Divider />
+                        <List>
+                            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                                <ListItem button key={text}>
+                                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItem>
+                            ))}
+                        </List>
+                        <Divider />
+                        <List>
+                            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                                <ListItem button key={text}>
+                                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </SwipeableDrawer>
+                </Container>
+
+                <ScrollTop {...this.props}>
+                    <Fab color="secondary" size="small" aria-label="scroll back to top">
+                        <KeyboardArrowUpIcon/>
+                    </Fab>
+                </ScrollTop>
+            </React.Fragment>
+        );
+    }
+}
+
+export default withStyles(styles)(App);
