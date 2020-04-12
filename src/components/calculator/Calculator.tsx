3809166e0ae3080601a87cebe904210d123b805d
Dan6erbond <moravrav@gmail.com>
2020-04-12 19:48:56 +0200

    Update calculator ui

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -9,7 +9,7 @@ import {
     TableHead,
     TableRow,
     TableCell,
-    TableBody, Button
+    TableBody, Button, Badge
 } from "@material-ui/core";
 import {styles} from "../styles";
 import {ReactCookieProps} from "react-cookie";
@@ -90,6 +90,7 @@ interface CalculatorState {
     subjects: Subject[];
     average: number;
     minusPoints: number;
+    projectMinusPoints: number;
 }
 
 export default class Calculator extends React.Component<WithStyles<typeof styles> & ReactCookieProps, CalculatorState> {
@@ -102,9 +103,9 @@ export default class Calculator extends React.Component<WithStyles<typeof styles
         let subjects: Subject[];
 
         const {cookies} = this.props;
+        subjects = cookies ? cookies.get("subjects") : undefined;
 
-        if (cookies) {
-            subjects = cookies.get("subjects");
+        if (subjects) {
             for (let i = 0; i < subjects.length; i++) {
                 let s = new Subject();
                 subjects[i] = s.copyInto(subjects[i]);
@@ -127,7 +128,7 @@ export default class Calculator extends React.Component<WithStyles<typeof styles
             ];
         }
 
-        this.state = {subjects: subjects, average: 0, minusPoints: 0};
+        this.state = {subjects: subjects, average: 0, minusPoints: 0, projectMinusPoints: 0};
     }
 
     componentDidMount() {
@@ -178,14 +179,16 @@ export default class Calculator extends React.Component<WithStyles<typeof styles
         }
 
         let pAvg = 0;
+        let pmp = 0;
 
-        if (pCnt !== 0 && fp !== 0){
+        if (pCnt !== 0 && fp !== 0) {
             let spAvg = pSum / pCnt;
             pAvg = (spAvg + fp) / 2;
             pAvg = Math.round(pAvg * 2) / 2;
 
             if (pAvg < 4)
-                mp += 4 - pAvg;
+                pmp = 4 - pAvg;
+            mp += pmp;
         }
 
         let avg = 0;
@@ -195,7 +198,7 @@ export default class Calculator extends React.Component<WithStyles<typeof styles
             console.log(`Average: ${avg}`);
         }
 
-        this.setState({average: avg, minusPoints: mp});
+        this.setState({average: avg, minusPoints: mp, projectMinusPoints: pmp});
     }
 
     render() {
@@ -207,68 +210,75 @@ export default class Calculator extends React.Component<WithStyles<typeof styles
 
                 <br/>
 
-                <form noValidate autoComplete="off">
-                    <TableContainer component={Paper}>
-                        <Table className={classes.table} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Fach</TableCell>
-                                    <TableCell align="right">1. Semester</TableCell>
-                                    <TableCell align="right">2. Semester</TableCell>
-                                    <TableCell align="right">3. Semester</TableCell>
-                                    <TableCell align="right">4. Semester</TableCell>
-                                    <TableCell align="right">5. Semester</TableCell>
-                                    <TableCell align="right">6. Semester</TableCell>
-                                    <TableCell align="right">Schlussprüfung</TableCell>
-                                    <TableCell align="right">Durchschnitt</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {this.state.subjects.map((s, i) => (
-                                    <TableRow key={i}>
-                                        <TableCell component="th" scope="row">
-                                            {s.name}
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Fach</TableCell>
+                                <TableCell align="right">1. Semester</TableCell>
+                                <TableCell align="right">2. Semester</TableCell>
+                                <TableCell align="right">3. Semester</TableCell>
+                                <TableCell align="right">4. Semester</TableCell>
+                                <TableCell align="right">5. Semester</TableCell>
+                                <TableCell align="right">6. Semester</TableCell>
+                                <TableCell align="right">Schlussprüfung</TableCell>
+                                <TableCell align="right">Durchschnitt</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.subjects.map((s, i) => (
+                                <TableRow key={i}>
+                                    <TableCell component="th" scope="row">
+                                        {s.name}
+                                    </TableCell>
+                                    {Array.from({length: 7}, (v, j) => (
+                                        <TableCell align="right" key={j}>
+                                            {s.grades[j] !== -1 ?
+                                                <TextField size="small" id={`grade-${i}-${j}`} label="Note"
+                                                           variant="outlined" value={s.grades[j]}
+                                                           inputProps={{min: "1", max: "6", step: "0.5"}}
+                                                           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                                               this.setGrade((event.target as HTMLInputElement).value, i, j)
+                                                           }}
+                                                           type="number" InputLabelProps={{shrink: true,}}/>
+                                                : null}
                                         </TableCell>
-                                        {Array.from({length: 7}, (v, j) => (
-                                            <TableCell align="right" key={j}>
-                                                {s.grades[j] !== -1 ?
-                                                    <TextField size="small" id={`grade-${i}-${j}`} label="Note"
-                                                               variant="outlined"
-                                                               inputProps={{min: "1", max: "6", step: "0.5"}}
-                                                               onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.setGrade((event.target as HTMLInputElement).value, i, j)}
-                                                               value={s.grades[j]}
-                                                               type="number" InputLabelProps={{shrink: true,}}/>
-                                                    : null}
-                                            </TableCell>
-                                        ))}
-                                        <TableCell align="right">
+                                    ))}
+                                    <TableCell align="right" style={{width: '140px'}}>
+                                        <Badge color="secondary"
+                                               badgeContent={-(i < this.state.subjects.length - 5 && s.average() > 0 ?
+                                                   Math.max(4 - s.average(), 0) : this.state.projectMinusPoints)}>
                                             <TextField size="small" id={`grade-${i}-average`} label="Durchschnitt"
                                                        variant="outlined" value={s.average()}
-                                                       InputProps={{readOnly: true,}} error={s.average() < 4}
-                                                       type="number" InputLabelProps={{shrink: true}}/>
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                                <TableRow>
-                                    <TableCell component="th" scope="row">Gesamtschnitt</TableCell>
-                                    {Array.from({length: 7}, (v, j) => (
-                                        <TableCell align="right" key={j}/>
-                                    ))}
-                                    <TableCell align="right">
-                                        <TextField size="small" id={`average`} label="Durchschnitt"
-                                                   variant="outlined" value={this.state.average}
-                                                   InputProps={{readOnly: true,}}
-                                                   type="number" InputLabelProps={{shrink: true}}/>
+                                                       InputProps={{readOnly: true,}}
+                                                       error={(i < this.state.subjects.length - 5 && s.average() < 4 && s.average() > 0) ||
+                                                       (i >= this.state.subjects.length - 5 && this.state.projectMinusPoints > 0)}
+                                                       type="number" InputLabelProps={{shrink: true,}}/>
+                                        </Badge>
                                     </TableCell>
                                 </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </form>
-
-                <br/>
-
-                <p>Minuspunkte: {this.state.minusPoints}</p>
+                            ))}
+                            <TableRow>
+                                <TableCell component="th" scope="row">Gesamtschnitt</TableCell>
+                                {Array.from({length: 7}, (v, j) => (
+                                    <TableCell align="right" key={j}/>
+                                ))}
+                                <TableCell align="right">
+                                    <TextField size="small" id={`average`} label="Durchschnitt"
+                                               variant="outlined" value={this.state.average.toPrecision(2)}
+                                               InputProps={{readOnly: true,}}
+                                               type="number" InputLabelProps={{shrink: true}}/>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="right" colSpan={9}>
+                                    <Typography variant="body1"
+                                                color={this.state.minusPoints <= 2 ? "textPrimary" : "error"}>Minuspunkte: {this.state.minusPoints}</Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
                 <br/>
 
