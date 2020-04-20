aca85623e9ffcee9dfc45cba38bd4eb2bc48f734
Dan6erbond <moravrav@gmail.com>
2020-04-20 07:59:46 +0200

    Add french subject/documents

--- src/components/subject/Subjects.tsx
+++ src/components/subject/Subjects.tsx
@@ -22,6 +22,16 @@ export default function Subjects() {
 
     return (
         <div className={classes.root}>
+            <Subject name={"Französisch"} expanded={expanded} handleChange={handleChange}>
+                <div>
+                    <DocumentContainer children={[
+                        {name: "Grammatik", path: "res/docs/french/franzoesisch_grammatik.pdf"},
+                        {name: "Verben", path: "res/docs/french/verben_envol_buecher.pdf"},
+                        {name: "Vokabular Relations Sociales", path: "res/docs/french/vocabulaire_relations_socialies_loisirs_vie_privee.pdf"},
+                        {name: "Lösungen Cours Intensif 2", path: "res/docs/french/cours_intensif_2_l.pdf"}
+                    ]}/>
+                </div>
+            </Subject>
         </div>
     );
 };
