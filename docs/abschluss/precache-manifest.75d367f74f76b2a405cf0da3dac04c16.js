4daaec6b62658621172205f4cff19267f6eda283
Dan6erbond <moravrav@gmail.com>
2020-05-15 13:41:54 +0200

    Add abschluss submodule

---
+++ docs/abschluss/precache-manifest.75d367f74f76b2a405cf0da3dac04c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee907954b6028fa592019c1bfbb7c208",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1c14fb1bdd789e51ad1",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a336219c71c024033865",
+    "url": "./static/js/2.43de128d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.43de128d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c14fb1bdd789e51ad1",
+    "url": "./static/js/main.9b120d95.chunk.js"
+  },
+  {
+    "revision": "bb1f7d5f28c77e7c3a4d",
+    "url": "./static/js/runtime-main.b1dfa127.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
